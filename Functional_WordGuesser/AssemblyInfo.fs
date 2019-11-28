7b76f33c5e089b12915abb467ef9174a58515190
Carlos <carl7755@easv365.dk>
2019-11-28 12:17:44 +0100

    init commit

---
+++ Functional_WordGuesser/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿namespace Functional_WordGuesser
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("Functional_WordGuesser")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
