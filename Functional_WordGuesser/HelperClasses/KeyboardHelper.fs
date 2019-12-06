9354c1c094ef0a250521e99afb408c0743ebe18a
Carlos <carl7755@easv365.dk>
2019-12-06 15:44:39 +0100

    Added keyboard helper

---
+++ Functional_WordGuesser/HelperClasses/KeyboardHelper.fs
@@ -0,0 +1,15 @@
+﻿module KeyboardHelper
+
+open System
+
+let GetKeysAndModifiers() : ConsoleKeyInfo =
+    Console.TreatControlCAsInput <- true
+
+    let cki = Console.ReadKey()
+    match cki.Modifiers with
+    | ConsoleModifiers.Alt -> printfn "ALT+"
+    | ConsoleModifiers.Shift -> printfn "SHIFT+"
+    | ConsoleModifiers.Control -> printfn "CTRL+"
+    | _ -> printfn "not recognized modifier"
+
+    cki
