9d56769d4a9166d4e53e56ef4c50641f0a7d97a4
PBLauge <peder.laugesen@gmail.com>
2019-12-08 17:27:39 +0100

    Removed redundant code

--- Functional_WordGuesser/HelperClasses/KeyboardHelper.fs
+++ Functional_WordGuesser/HelperClasses/KeyboardHelper.fs
@@ -6,10 +6,5 @@ let GetKeysAndModifiers() : ConsoleKeyInfo =
     Console.TreatControlCAsInput <- true
 
     let cki = Console.ReadKey()
-    match cki.Modifiers with
-    | ConsoleModifiers.Alt -> printfn ""
-    | ConsoleModifiers.Shift -> printfn ""
-    | ConsoleModifiers.Control -> printfn ""
-    | _ -> printfn ""
 
     cki
