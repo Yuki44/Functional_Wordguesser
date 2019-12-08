79a7499d232122d1d9f6df80d3ba849bce03a98c
Carlos <carl7755@easv365.dk>
2019-12-08 14:39:22 +0100

    Those printfn were really annoying, so I now they are not shown

--- Functional_WordGuesser/HelperClasses/KeyboardHelper.fs
+++ Functional_WordGuesser/HelperClasses/KeyboardHelper.fs
@@ -7,9 +7,9 @@ let GetKeysAndModifiers() : ConsoleKeyInfo =
 
     let cki = Console.ReadKey()
     match cki.Modifiers with
-    | ConsoleModifiers.Alt -> printfn "ALT+"
-    | ConsoleModifiers.Shift -> printfn "SHIFT+"
-    | ConsoleModifiers.Control -> printfn "CTRL+"
-    | _ -> printfn "not recognized modifier"
+    | ConsoleModifiers.Alt -> printfn ""
+    | ConsoleModifiers.Shift -> printfn ""
+    | ConsoleModifiers.Control -> printfn ""
+    | _ -> printfn ""
 
     cki
