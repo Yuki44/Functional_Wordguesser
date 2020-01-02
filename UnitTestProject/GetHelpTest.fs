2fdc0875bd24aa3007c92585d01a5366c4289d13
PBLauge <peder.laugesen@gmail.com>
2020-01-02 12:14:10 +0100

    Test project added
    I added tests for GetHelp and the toPartialWord method from the logic file.

---
+++ UnitTestProject/GetHelpTest.fs
@@ -0,0 +1,25 @@
+﻿module GetHelpTest
+
+open Xunit
+
+[<Fact>]
+let ``Get help for word`` () =
+    //Arrange
+    let wordToGuess1 = "mountain"
+    let wordGuessedSoFar1 = "***ntain"
+    let lettersUsedSoFar1 = ['n';'t';'a';'i';'z']
+    //Act
+    let helpingLetter1 = GetHelp.HelpLetter(wordGuessedSoFar1)(wordToGuess1)(lettersUsedSoFar1)
+    //Assert
+    Assert.True(wordToGuess1.Contains(helpingLetter1))
+
+[<Fact>]
+let ``Get help for word 2`` () =
+    //Arrange
+    let wordToGuess2 = "full house"
+    let wordGuessedSoFar2 = "**ll **us*"
+    let lettersUsedSoFar2 = ['l';'y';'c';'k';'z';'u';'s']
+    //Act
+    let helpingLetter2 = GetHelp.HelpLetter(wordGuessedSoFar2)(wordToGuess2)(lettersUsedSoFar2)
+    //Assert
+    Assert.True(wordToGuess2.Contains(helpingLetter2))
---
+++ UnitTestProject/LogicTest.fs
@@ -0,0 +1,24 @@
+﻿module LogicTest
+
+open Xunit
+open Logic
+
+[<Fact>]
+let ``Set letters as hidden without a white space`` () =
+    //Arrange
+    let wordToGuess = "manager"
+    let lettersUsedSoFar = []
+    //Act
+    let hiddenWord = toPartialWord (wordToGuess)(lettersUsedSoFar)
+    //Assert
+    Assert.Equal("*******", hiddenWord)
+
+[<Fact>]
+let ``Set letters as hidden with a white space`` () =
+    //Arrange
+    let wordToGuess = "official statement"
+    let lettersUsedSoFar = []
+    //Act
+    let hiddenWord = toPartialWord (wordToGuess)(lettersUsedSoFar)
+    //Assert
+    Assert.Equal("******** *********", hiddenWord)
